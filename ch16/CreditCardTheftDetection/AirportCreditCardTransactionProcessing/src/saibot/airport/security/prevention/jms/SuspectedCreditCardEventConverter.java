a7ac2441df98a6acef5b14d46c5658b526be51e5
lucasjellema <lucasjellema@gmail.com>
2014-09-15 23:32:48 +0200

    Added JMS outbound with a JMS converter bean; also note: two bundles were added to the manifest.mf file ; the second jms adapter is configured in processor.xml (without event type) and the converter bean is configured in the spring context file.

--- ch16/CreditCardTheftDetection/AirportCreditCardTransactionProcessing/src/saibot/airport/security/prevention/jms/SuspectedCreditCardEventConverter.java
+++ ch16/CreditCardTheftDetection/AirportCreditCardTransactionProcessing/src/saibot/airport/security/prevention/jms/SuspectedCreditCardEventConverter.java
@@ -1,9 +1,12 @@
 package saibot.airport.security.prevention.jms;
+
 import com.bea.wlevs.adapters.jms.api.MessageConverterException;
 import com.bea.wlevs.adapters.jms.api.OutboundMessageConverter;
+
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,25 +14,40 @@ import javax.jms.MapMessage;
 
 import saibot.airport.security.prevention.event.SuspectCreditCardEvent;
 
-public class SuspectedCreditCardEventConverter  //implements OutboundMessageConverter 
-                                            {
+public class SuspectedCreditCardEventConverter implements OutboundMessageConverter {
     public SuspectedCreditCardEventConverter() {
         super();
     }
+
+    public List<Message> convert(Session session, Object inputEvent) throws MessageConverterException, JMSException {
+        SuspectCreditCardEvent event = (SuspectCreditCardEvent) inputEvent;
+        MapMessage message = session.createMapMessage();
+        message.setString("creditCardNumber", event.getCreditCardNumber());
+        message.setString("listOfShops", intListCSV(event.getShops()));
+        message.setString("listOfAmounts", floatListCSV(event.getPurchaseAmounts()));
+        message.setFloat("totalSum", event.getSumAmounts());
+        message.setFloat("durationOfShoppingSpree", event.getShoppingDuration());
+        
+
+        List<Message> messages = new ArrayList<Message>();
+        messages.add(message);
+        return messages;
+    }
     
-    public List<Message> convert(Session session, Object inputEvent) 
-           throws 
-    // MessageConverterException, 
-     JMSException 
-    {
-            SuspectCreditCardEvent event = (SuspectCreditCardEvent) inputEvent;
-            MapMessage message = session.createMapMessage();
-            message.setString("CreditCardNumber", event.getCreditCardNumber());
-            message.setFloat("SumAmounts", event.getSumAmounts());
-            message.setFloat("ShoppingDuration", event.getShoppingDuration());
-            
-            List<Message> messages = new ArrayList<Message>();
-            messages.add(message);
-            return messages;
+    public String intListCSV(List<Integer> intList) {
+        String intsString = "";
+        for (Integer intId:intList) {
+            intsString = intsString+","+intId;
         }
+        // remove initial comma
+        return intsString.substring(1);
+    }
+    public String floatListCSV(List<Float> floatList) {
+        String floatsString = "";
+        for (Float floatId:floatList) {
+            floatsString = floatsString+","+floatId;
+        }
+        // remove initial comma
+        return floatsString.substring(1);
+    }
 }
